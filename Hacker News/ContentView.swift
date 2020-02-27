787cbd52a58eef37419f26288adff14dd7b29e42
maxakash <akashchaudharymax@gmail.com>
2020-02-27 19:37:48 +0530

    Initial Commit

---
+++ Hacker News/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Hacker News
+//
+//  Created by Akash Chaudhary on 27/02/20.
+//  Copyright © 2020 Akash Chaudhary. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
